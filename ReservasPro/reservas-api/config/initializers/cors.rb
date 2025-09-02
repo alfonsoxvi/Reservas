a6b4a283b95f05089bb78102c8bf58662b2fc515
alfonsoxvi <alpho6875@gmail.com>
2025-09-01 22:44:03 -0400

    Update cors.rb

--- ReservasPro/reservas-api/config/initializers/cors.rb
+++ ReservasPro/reservas-api/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://reservas-1.onrender.com/'  # tu frontend React
+    origins 'https://reservas-1.onrender.com'  # tu frontend React
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
